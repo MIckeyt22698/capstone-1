b57d033d7072875e5b3947ef44000ca935168bdc
Melkias_Tekeste <mickeyt22698@gmail.com>
2025-04-25 15:43:01 -0400

    LearnToCode_Capstone/

--- LearnToCode_Capstone/src/main/java/com/pluralsight/App.java
+++ LearnToCode_Capstone/src/main/java/com/pluralsight/App.java
@@ -1,7 +1,9 @@
 package com.pluralsight;
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+
+
+        
     }
 }
